fdc860c8667ee768849e5cc55e728d8346d4472c
ssakthivel02 <ssakthivel02@gmail.com>
2026-07-19 23:01:28 +0100

    feat: add leadership navigation

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -12,6 +12,7 @@ export function SiteHeader() {
         <a href="/operations/">Operations</a>
         <a href="/governance/">Governance</a>
         <a href="/delivery/">Delivery</a>
+        <a href="/leadership/">Leadership</a>
         <a href="/tracks/">Tracks</a>
         <a href="/search/">Search</a>
         <a href="/dashboard/">Dashboard</a>
